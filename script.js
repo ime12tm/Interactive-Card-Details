334602f41a961487ce64d006f1df54254e774b03
ime12tm <ime12tm@yahoo.com>
2023-11-30 00:38:16 +0200

    update js code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sectionRight = document.querySelector(".section-right");
+const CCForm = document.querySelector("#CCForm");
 const cardName = document.getElementById("cardName");
 const cardNumber = document.querySelector("#cardNumber");
 const expiryMonth = document.getElementById("expiryMonth");
@@ -14,35 +14,109 @@ const CCMonth = document.querySelector("#CCMonth");
 const CCYear = document.querySelector("#CCYear");
 const CVVNumber = document.querySelector(".CVV-number");
 
-// if (cardNumber.length !== 16 || !/^\d+$/.test(cardNumber)) {
-//   alert("Invalid card number. Please enter a 16-digit number.");
-//   return;
-// }
-cardName.addEventListener("input", function () {
-  if (cardName.length > 5) {
-    CCName.textContent = "Jane Appleseed";
+cardName.addEventListener("input", function (event) {
+  const errorName = document.getElementById("errorName");
+  CCName.textContent = cardName.value;
+  if (cardName.value.length >= 16) {
+    errorName.innerHTML = "Name must be less than 16 characters.";
   } else {
-    CCName.textContent = cardName.value;
+    errorName.innerHTML = "";
   }
 });
 
-cardNumber.addEventListener("input", function () {
+cardNumber.addEventListener("input", function (event) {
   CCNumber.textContent = cardNumber.value;
 });
+
 expiryMonth.addEventListener("input", function () {
-  CCMonth.textContent = expiryMonth.value;
+  const errorMonth = document.getElementById("errorMonth");
+  if (isNumericDate(expiryMonth.value)) {
+    CCMonth.textContent = expiryMonth.value;
+    errorDate.innerHTML = "";
+  } else {
+    CCMonth.textContent = "00";
+    errorDate.innerHTML = "Please enter a valid month.";
+  }
 });
+
 expiryYear.addEventListener("input", function () {
-  CCYear.textContent = expiryYear.value;
+  const errorYear = document.getElementById("errorYear");
+  if (isNumericDate(expiryYear.value)) {
+    CCYear.textContent = expiryYear.value;
+    errorDate.innerHTML = "";
+  } else {
+    CCYear.textContent = "00";
+    errorDate.innerHTML = "Please enter a valid year.";
+  }
 });
+
 cvv.addEventListener("input", function () {
-  CVVNumber.textContent = cvv.value;
+  const errorCVV = document.getElementById("errorCVV");
+
+  if (isNumericCVV(cvv.value)) {
+    CVVNumber.textContent = cvv.value;
+    errorCVV.innerHTML = "";
+  } else {
+    CVVNumber.textContent = "123";
+    errorCVV.innerHTML = "Please enter a valid CVV.";
+  }
 });
+
 confirmBtn.addEventListener("click", function () {
-  sectionRight.style.display = "none";
   completeSection.style.display = "block";
+  CCForm.style.display = "none";
 });
+
 completeBtn.addEventListener("click", function () {
   completeSection.style.display = "none";
-  sectionRight.style.display = "block";
+  CCForm.style.display = "block";
+  resetData();
 });
+
+cardNumber.addEventListener("input", function (event) {
+  let cardNumber = event.target.value;
+  const errorNumber = document.getElementById("errorNumber");
+  cardNumber = cardNumber
+    .replace(/\D/g, "")
+    .replace(/(\d{4})/g, "$1 ")
+    .trim()
+    .substr(0, 19);
+  event.target.value = cardNumber;
+  if (!/\D/g.test(cardNumber)) {
+    cardNumber.textContent = "";
+    errorNumber.innerHTML = "Please enter a valid card number.";
+  } else {
+    errorNumber.innerHTML = "";
+  }
+});
+
+function resetData() {
+  cardName.value = "";
+  cardNumber.value = "";
+  expiryMonth.value = "";
+  expiryYear.value = "";
+  cvv.value = "";
+  // CCName.textContent = cardName.placeholder;
+  CCName.textContent = "Jane Appleseed";
+  CCNumber.textContent = "0000 0000 0000 0000";
+  CCMonth.textContent = "00";
+  CCYear.textContent = "00";
+  CVVNumber.textContent = "123";
+}
+// function onLoad() {
+//   resetData();
+// }
+function isNumericDate(date) {
+  // Check if the input is a number
+  const isNumber = !isNaN(date) && !isNaN(parseFloat(date));
+  // Check if the number is an integer between 1 and 12
+  const isValidDate = isNumber && Number.isInteger(Number(date)) && Number(date) >= 1 && Number(date) <= 99;
+  return isValidDate;
+}
+function isNumericCVV(num) {
+  // Check if the input is a number
+  const isNumber = !isNaN(num) && !isNaN(parseFloat(num));
+  // Check if the number is an integer between 1 and 12
+  const isValidDate = isNumber && Number.isInteger(Number(num)) && Number(num) >= 1 && Number(num) <= 999;
+  return isValidDate;
+}
